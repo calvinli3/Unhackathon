27049d4acb6bf75992a33751a1b024b125a359b9
cli473 <calvin.li@stonybrook.edu>
2016-01-14 21:58:28 -0500

    Delete ex2.py

--- ex2.py
+++
@@ -1,6 +0,0 @@
-# comment asdfasdfasdf test.
-print "I could have code like this." # and it would run
-# comments will disable shit lolxd
-print "This will run."
-
-#lol im a fuckin lazy ass bich xdeee
